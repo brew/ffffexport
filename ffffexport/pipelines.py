8afd421568167af2e295bfc0bab418aac22394d3
Brook Elgie <specialbrew@gmail.com>
2014-04-30 11:27:48 +0100

    Move url scheme prefixing to pipeline

--- ffffexport/pipelines.py
+++ ffffexport/pipelines.py
@@ -1,6 +1,11 @@
+import urlparse
+
+from scrapy.http import Request
 from scrapy.contrib.pipeline.images import ImagesPipeline
 
 
 class FfffoundImagesPipeline(ImagesPipeline):
 
-    pass
+    def get_media_requests(self, item, info):
+        # Prefix http:// (missing from scrapped data)
+        return [Request(urlparse.urlparse(x, scheme='http').geturl().replace('http:///', 'http://')) for x in item.get(self.IMAGES_URLS_FIELD, [])]
